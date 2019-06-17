95a89f101722d072f2ac5a5d03e60ccc465642b6
Rob Hedrick <rob@hedrickholdings.com>
2019-06-17 18:54:26 -0500

    debug custom device_name

--- action-wizbowes-turn_on-wizbowes.HomeAssistant_via_REST_API.py
+++ action-wizbowes-turn_on-wizbowes.HomeAssistant_via_REST_API.py
@@ -61,7 +61,6 @@ def action_wrapper(hermes, intentMessage, conf):
        print 'http://'+ myip.encode("utf-8") + ':' + myport.encode("utf-8") + '/api/states/' + myDeviceId.encode("utf-8")
        print myDeviceName.encode("utf-8")
        print  myDeviceId.encode("utf-8")
-       print intentMessage.slots.device_name[0].value
        hermes.publish_end_session(current_session_id, "Sorry, something went wrong again")
 
  
